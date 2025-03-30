c896e8c6e792be5d4ff38dcd6b52bede4941b2a5
electronaught <jamoore84@gmail.com>
2025-03-30 10:38:53 -0600

    moved code around

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,53 +1,4 @@
-const deadline = 'April 30 2025 23:59:59 GMT-0700';
 
-
-
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-       
-        minutes = Math.floor(timer / 60000);
-        seconds = Math.floor((timer % 60000)/1000);
-
-        //zero pad digits below 10
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-         if (--timer > 0) {
-              timer = Date.parse(deadline)-Date.now();
-          }    
-        
-    }, 1000);
- 
-}
-
-function init(display){
-    var loop = 0;
-    var id = setInterval(function () {
-        
-        if (loop>15){
-            clearInterval(id)
-        }
-
-        var rsec=Math.floor(Math.random()*60)
-        var rmin=Math.floor(Math.random()*90000)+10000
-
-        rsec = rsec < 10 ? "0" + rsec : rsec;
-
-        display.textContent = rmin + ":" + rsec;
-       loop++;
-    }, 100);
-}
-
-window.onload = function () {
-    display = document.querySelector('#countdown');
-    init(display);
-    var remaining = Date.parse(deadline)-Date.now();
-    startTimer(remaining, display);
-};
 
 // scripts.js
 
@@ -87,11 +38,16 @@ var form = document.getElementById('contactForm').addEventListener('submit', fun
         // You can also submit the form here using AJAX or similar methods
         resetFields();
     }
+
+    return false;
 });
 
 
 
 function resetFields() {
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
+    const nameField= document.getElementById('name');
+    console.log(nameField)
+    nameField.value='0000'
+    console.log(nameField);
+    document.getElementById('email').textContent = '';
 }
---
+++ js/main.js
@@ -0,0 +1,48 @@
+const deadline = 'April 30 2025 23:59:59 GMT-0700';
+
+window.onload = function () {
+    var remaining = Date.parse(deadline)-Date.now();
+    display = document.querySelector('#countdown');
+    init(display);
+    startTimer(remaining, display);
+};
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+       
+        minutes = Math.floor(timer / 60000);
+        seconds = Math.floor((timer % 60000)/1000);
+
+        //zero pad digits below 10
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+         if (--timer > 0) {
+              timer = Date.parse(deadline)-Date.now();
+          }    
+        
+    }, 1000);
+ 
+}
+
+function init(display){
+    var loop = 0;
+    var id = setInterval(function () {
+        
+        if (loop>15){
+            clearInterval(id)
+        }
+
+        var rsec=Math.floor(Math.random()*60)
+        var rmin=Math.floor(Math.random()*90000)+10000
+
+        rsec = rsec < 10 ? "0" + rsec : rsec;
+
+        display.textContent = rmin + ":" + rsec;
+       loop++;
+    }, 100);
+}
+
